1171c296b9e31a0f7d46749a7fa2972415b4b5ef
AlexDoss42 <alex.doss42@gmail.com>
2021-03-31 16:02:06 -0600

    adds an event listener for no and backdrop

---
+++ shared.js
@@ -0,0 +1,22 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative')
+var selectPlanButton = document.querySelectorAll('.plan button');
+
+
+for (var i = 0; i < selectPlanButton.length; i++) {
+  selectPlanButton[i].addEventListener('click', function() {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+  });
+}
+
+noButton.addEventListener('click', function() {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
+});
+
+backdrop.addEventListener('click', function() {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
+});

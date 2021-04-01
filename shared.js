d6d8856000a15e63738bd24b4d201d0a6fb290d7
AlexDoss42 <alex.doss42@gmail.com>
2021-04-01 11:21:53 -0600

    adds close open mobile nav buttons

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative')
 var selectPlanButton = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
 
 
 for (var i = 0; i < selectPlanButton.length; i++) {
@@ -12,11 +14,17 @@ for (var i = 0; i < selectPlanButton.length; i++) {
 }
 
 noButton.addEventListener('click', function() {
-  modal.style.display = 'none';
-  backdrop.style.display = 'none';
+  mobileNav.style.display = 'none';
+  this.closeModal();
 });
 
-backdrop.addEventListener('click', function() {
+backdrop.addEventListener('click', closeModal());
+
+function closeModal() {
   modal.style.display = 'none';
   backdrop.style.display = 'none';
-});
+}
+
+toggleButton.addEventListener('click', function() {
+  mobileNav.style.display = 'block';
+})
